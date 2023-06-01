7b15d2c59da63478545c5958ac160cec8c6bb2b5
marcus-biddle <marcusbiddle16@gmail.com>
2023-06-01 14:05:02 -0700

    fixed weird css styling.

--- schs-client/src/App.tsx
+++ schs-client/src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {Navbar} from './components/Navbar'
+import { Navbar } from './components/Navbar'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div>Test</div>
+    <div>
       <Navbar />
-    </>
+      <div >Outlet</div>
+    </div>
   )
 }
 
--- schs-client/src/main.tsx
+++ schs-client/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { ErrorPage } from './pages/ErrorPage.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
