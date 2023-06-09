e5d8001f1f27afc85cbe2da3553a607deda4be0f
Mars <marcusbiddle16@gmail.com>
2023-06-08 19:04:47 -0700

    runner progress

--- schs-client/src/helpers/index.ts
+++ schs-client/src/helpers/index.ts
@@ -1,4 +1,10 @@
-export const urlContains = (url: string, searchTexts: string[]): boolean => {
-    return searchTexts.some((searchText) => url.includes(searchText));
-  };
+export const urlContains = (url: string, searchTexts: string[]): string | null => {
+  const foundText = searchTexts.find((searchText) => {
+    const regex = new RegExp(`\\b${searchText}\\b`, 'i');
+    return regex.test(url);
+  });
+
+  return foundText || null;
+};
+
   
--- schs-client/src/pages/XC/Runners/index.tsx
+++ schs-client/src/pages/XC/Runners/index.tsx
@@ -2,13 +2,13 @@ import { useLocation } from "react-router"
 import { urlContains } from "../../../helpers"
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { fetchAthletes } from "../../../api/athletes";
+import { Athlete, fetchAthletes } from "../../../api/athletes";
 
 export const Runners = () => {
     // get the url, check if xc or not then use correct data for page
     const location = useLocation();
-    const XCPage: boolean = urlContains(location.pathname, ['cross-country']);
-    const genderFilter: boolean = urlContains(location.pathname, ['men', 'women']);
+    const XCPage: string | null = urlContains(location.pathname, ['cross-country']);
+    const genderFilter: string | null= urlContains(location.pathname, ['men', 'women']);
     const [athletes, setAthletes] = useState([]);
 
     useEffect(() => {
@@ -17,13 +17,13 @@ export const Runners = () => {
         .catch((error: any) => console.error('Error fetching athletes:', error));
     }, []);
 
-    console.log(athletes);
+    console.log(genderFilter, athletes.filter((athlete: Athlete) => genderFilter === "women" ? athlete.genderId === 3 : genderFilter === "men" ? athlete.genderId === 2 : athlete));
 
   return (
     <div style={{ marginLeft: '10rem', marginRight: '10rem'}}>
         { XCPage ?
             <>
-                <h2>SCHS Cross Country Runners (<span>{athletes.length}</span>)</h2>
+                <h2>SCHS Cross Country Runners (<span>{athletes.length}</span>) {`- ${genderFilter ? genderFilter : ''}`}</h2>
                 {!genderFilter && <div>
                     <span>
                         <Link to={'men/'}>Men</Link>
@@ -33,15 +33,15 @@ export const Runners = () => {
                     </span>
                 </div>}
                 <ol className="athlete-list">
-                {/* {runnerData.map((athlete) => (
+                {athletes.filter((athlete: Athlete) => genderFilter === "women" ? athlete.genderId === 3 : genderFilter === "men" ? athlete.genderId === 2 : athlete).map((athlete: Athlete) => (
                     // We can update this accordingly when we have real data with gender and names so we can filter it better
-                    <Link to={athlete.gender === 1 ? `men/${athlete.id}` : `women/${athlete.id}`}>
-                    <li key={athlete.name} className="athlete-item">
-                    <span>{athlete.name}</span>
+                    <Link to={athlete.genderId === 2 ? `men/${athlete.athleteId}` : `women/${athlete.athleteId}`}>
+                    <li key={athlete.athleteId} className="athlete-item">
+                    <span>{athlete.firstName} {athlete.lastName}</span>
                     </li>
                     </Link>
                     
-                ))} */}
+                ))}
                 </ol>
             </>
         : 
