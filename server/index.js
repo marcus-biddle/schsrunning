9b22b2f49e8ffdc815a967872f893ced75fd9b4d
marcus-biddle <marcusbiddle16@gmail.com>
2023-07-19 12:46:27 -0700

    added header

--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Content-Security-Policy', "default-src 'self'");
   next();
 });
 
