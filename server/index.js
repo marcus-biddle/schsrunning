042753c1d37bfa4a6d9fa70f1cf95eeb1902bd1e
marcus-biddle <marcusbiddle16@gmail.com>
2023-07-19 12:47:09 -0700

    .

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Content-Security-Policy', "default-src 'self'");
-  next();
+  next(); 
 });
 
 const PORT = process.env.PORT || 3000;
