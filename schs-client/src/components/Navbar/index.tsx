3b647d04d6e20958e8ddfe313f0f4120fd5277e8
marcus-biddle <marcusbiddle16@gmail.com>
2023-07-20 15:32:04 -0700

    navbar

--- schs-client/src/components/Navbar/index.tsx
+++ schs-client/src/components/Navbar/index.tsx
@@ -3,24 +3,27 @@ import { Link, useLocation } from 'react-router-dom';
 import './styled/index.css';
 import { urlContains } from '../../helpers';
 import { deleteAccessTokenCookie } from '../../authUtils';
+import logo from '../../assets/logo.png';
+import { SearchInput } from '../SearchFeatures/SearchInput';
+import { GenericButton } from '../Button';
 
 export const Navbar: React.FC = () => {
   const location = useLocation();
-  const isAdminPage = urlContains(location.pathname, ['admin'])
-  const handleLogout = () => {
-    deleteAccessTokenCookie();
-  };
+  // const isAdminPage = urlContains(location.pathname, ['admin'])
+  // const handleLogout = () => {
+  //   deleteAccessTokenCookie();
+  // };
 
   return (
     <nav className="navbar">
-      {isAdminPage ? 
+      {/* {isAdminPage ? 
       <div className="nav-container">
       <Link to="/admin/dashboard" className="navbar__link">
         Athletes
       </Link>
-      {/* <Link to="/admin/athletes" className="navbar__link">
+      <Link to="/admin/athletes" className="navbar__link">
         Athletes
-      </Link> */}
+      </Link>
       <Link to="/admin/xc-races" className="navbar__link">
         XC Races
       </Link>
@@ -31,21 +34,42 @@ export const Navbar: React.FC = () => {
         Logout
       </Link>
     </div>
-      :
-      <div className="nav-container">
-        <Link to="/" className="navbar__link">
-          Home
-        </Link>
-        <Link to="santa-clara-high-cross-country/" className="navbar__link">
-          XC
-        </Link>
-        <Link to="santa-clara-high-track-and-field/" className="navbar__link">
-          Track
-        </Link>
-        <Link to="/workouts" className="navbar__link">
-          Workouts
-        </Link>
-      </div>}
+      : */}
+        {/* left side of navbar */}
+        {/* <div className="nav-container-left" >
+          <div style={{ position: 'relative', height: '32px', width: '56px', overflow: 'hidden', display: 'flex', justifyContent: 'start'}}>
+            <img src={logo} alt='schs logo' style={{ height: '4.5rem', width: '4rem', position: 'absolute', top: '-18px', right: '-4px'}}/>
+          </div>
+          <div>
+            <Link to="/" className="navbar__link">
+              Home
+            </Link>
+            <Link to="santa-clara-high-cross-country/" className="navbar__link">
+              XC
+            </Link>
+            <Link to="santa-clara-high-track-and-field/" className="navbar__link">
+              Track
+            </Link>
+            <Link to="/workouts" className="navbar__link">
+              Workouts
+            </Link>
+          </div>
+        </div> */}
+        {/* Right side of navbar */}
+        {/* <div style={{ display: 'flex', backgroundColor: 'gray'}}>
+          
+          <input
+              type="text"
+              placeholder="Search Athletes"
+              value={''}
+              onChange={() => null}
+              className="search-input"
+          />
+          <button>Search</button>
+
+        </div> */}
+      
+      {/* } */}
     </nav>
   );
 };
