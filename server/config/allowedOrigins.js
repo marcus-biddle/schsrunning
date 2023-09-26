14935a0bed543988c1047b2ca162aae497e8913a
marcus-biddle <marcusbiddle16@gmail.com>
2023-09-26 08:44:28 -0700

    changed cors allowed config

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,4 @@
 export const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:3000',
-    'http://127.0.0.1:5500',
+    'https://schs-server.onrender.com',
     'https://schsrunning.vercel.app',
   ];
